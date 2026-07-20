7fb4d4d6df66ea4972f8e096a894dca76b205924
Zecheng_6114 <Zecheng_6114@outlook.com>
2026-07-21 02:12:57 +0800

    fix(install.sh): 修复archlinux密钥环安装和签名校验问题
    1. 移除重复的archlinux-keyring安装
    2. 添加SigLevel配置关闭签名校验

--- install.sh
+++ install.sh
@@ -123,8 +123,9 @@ cat >> /etc/pacman.conf <<'CNREPO'
 
 [archlinuxcn]
 Server = https://mirrors.zju.edu.cn/archlinuxcn/\$arch
+SigLevel = Never
 CNREPO
-pacman -Sy --noconfirm archlinux-keyring archlinuxcn-keyring paru
+pacman -Sy --noconfirm archlinuxcn-keyring paru
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
